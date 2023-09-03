8964b08a17a7d4631dd843e6f2cb4503465858c2
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-09-03 19:45:55 +0900

    オープンクローズド原則のbadExample

---
+++ 03-SOLID原則/オープンクローズド原則/badExample.ts
@@ -0,0 +1,48 @@
+export {}
+
+/*
+- オープンクローズドの原則
+- 拡張に対しては開いていて、修正に対しては閉じている
+- つまり、既存のコードを変更せずに、新しい機能を追加できるようにする
+- つまり、
+*/
+
+// 従業員の職位
+type Grade = "junior" | "middle" | "senior";
+
+class Employee {
+  constructor(
+    public name: string,
+    public grade: Grade,
+  ) {}
+}
+
+class BonusCalculator {
+  constructor(
+    public base: number
+  ) {}
+
+  getBonus(employee: Employee): number {
+    if (employee.grade === "junior") {
+      return Math.floor(this.base * 1.1);
+    } else if (employee.grade === "middle") {
+      return Math.floor(this.base * 1.5);
+    } else {
+      return Math.floor(this.base * 2);
+    }
+  }
+}
+
+function run() {
+  const emp1 = new Employee("山田", "junior");
+  const emp2 = new Employee("田中", "middle");
+  const emp3 = new Employee("佐藤", "senior");
+
+  const bonusCalculator = new BonusCalculator(100);
+
+  console.log(`${emp1.name}のボーナスは${bonusCalculator.getBonus(emp1)}円です`);
+  console.log(`${emp2.name}のボーナスは${bonusCalculator.getBonus(emp2)}円です`);
+  console.log(`${emp3.name}のボーナスは${bonusCalculator.getBonus(emp3)}円です`);
+}
+
+run()
