5c2e2ed852879fe7af27c41049de2ec20ae184a4
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-09-05 23:37:44 +0900

    リスコフの置換原則badExample

---
+++ 03-SOLID原則/リスコフの置換原則/badExample.ts
@@ -0,0 +1,59 @@
+export {}
+
+/*
+- リスコフの置換原則
+- サブクラスはそのスーパークラスと置換可能でなければならない
+- スーパークラスとサブクラスの継承関係が間違っている場合は、意図しない振る舞いをする
+- つまり、リスコフの置換原則に違反している場合は、継承関係が正しくない
+- 例：下の例だと、継承しているのにも関わらず、長方形の代わりに正方形を使うと意図しない振る舞いをする
+- つまり、利用者はスーパータイプとサブタイプは同じ挙動を期待しているのに、サブタイプを理解した上で利用しなければならない
+- 正しい継承 = 「is-a関係」 + 「振る舞いの同等性」
+*/
+
+
+// スーパークラス
+class Rectangle {
+  width = 0;
+  height = 0;
+
+  setWidth(width: number) {
+    this.width = width;
+  }
+
+  setHeight(height: number) {
+    this.height = height;
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+// サブクラス
+class Square extends Rectangle {
+  setWidth(width: number) {
+    super.setWidth(width)
+    super.setHeight(width)
+  }
+
+  setHeight(height: number) {
+    super.setWidth(height)
+    super.setHeight(height)
+  }
+}
+
+function f(r: Rectangle, width: number, height: number): number {
+  r.setWidth(width);
+  r.setHeight(height);
+  return r.getArea();
+}
+
+function run() {
+  const r1: Rectangle = new Rectangle();
+  const r2: Rectangle = new Square();
+
+  console.log(f(r1, 3, 4));
+  console.log(f(r2, 3, 4));
+}
+
+run()
