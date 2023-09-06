ede5aec3e985d6c109bd87a25eafd9555065dafa
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-09-06 18:56:57 +0900

    リスコフの置換原則、goodExample

---
+++ 03-SOLID原則/リスコフの置換原則/goodExample.ts
@@ -0,0 +1,54 @@
+export {}
+
+interface Shape {
+  getArea(): number;
+}
+
+class Rectangle implements Shape {
+  private width = 0;
+  private height = 0;
+
+  setWidth(width: number) {
+    this.width = width;
+  }
+
+  setHeight(height: number) {
+    this.height = height;
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+class Square implements Shape {
+  private length = 0;
+
+  setLength(length: number) {
+    this.length = length;
+  }
+
+  getArea(): number {
+    return this.length * this.length;
+  }
+}
+
+function f(shape: Shape) {
+  // ポリモーフィズム：一つのコードで複数のコードに切り替えられること
+  // つまり、shapeはRectangleかSquareのどちらかだが、どちらかを意識する必要がない
+  // これはポリモーフィズムを利用している
+  console.log(shape.getArea());
+}
+
+function run() {
+  const r1 = new Rectangle();
+  r1.setWidth(3);
+  r1.setHeight(4);
+  f(r1);
+
+  const r2 = new Square();
+  r2.setLength(3);
+  f(r2);
+}
+
+run()
