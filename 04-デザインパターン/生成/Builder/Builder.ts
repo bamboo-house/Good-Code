1bfced643d1a07b8112017c23073b6ec4a5c3977
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-09-18 13:58:36 +0900

    Builderパターン実装

--- 04-デザインパターン/生成/Builder/Builder.ts
+++ 04-デザインパターン/生成/Builder/Builder.ts
@@ -11,3 +11,86 @@ export {}
   - サブクラスが大量にある場合
   - コンストラクタの引数が多い場合
 */
+
+class Computer {
+  type: string | undefined;
+  cpu: string | undefined;
+  ram: number | undefined;
+}
+
+interface ComputerBuilder {
+  addCpu(cpu: string): void;
+  addRam(ram: number): void;
+}
+
+class DesktopBuilder implements ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+    this.computer.type = "Desktop";
+  }
+
+  addCpu(cpu: string) {
+    this.computer.cpu = cpu;
+  }
+
+  addRam(ram: number) {
+    this.computer.ram = ram;
+  }
+
+  getResult(): Computer {
+    return this.computer;
+  }
+}
+
+class LaptopBuilder implements ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+    this.computer.type = "Laptop";
+  }
+
+  addCpu(cpu: string) {
+    this.computer.cpu = cpu;
+  }
+
+  addRam(ram: number) {
+    this.computer.ram = ram;
+  }
+
+  getResult(): Computer {
+    return this.computer;
+  }
+}
+
+class Director {
+  constructor(private builder: ComputerBuilder) {}
+
+  construct() {
+    this.builder.addCpu("Intel Core i5");
+    this.builder.addRam(16);
+  }
+
+  highSpecConstruct() {
+    this.builder.addCpu("Intel Core i9");
+    this.builder.addRam(64);
+  }
+}
+
+function run() {
+  const desktopBuilder = new DesktopBuilder();
+  const desktopDirector = new Director(desktopBuilder);
+  desktopDirector.construct();
+  const desktopComputer = desktopBuilder.getResult();
+  console.log(desktopComputer);
+
+  const laptopBuilder = new LaptopBuilder();
+  const laptopDirector = new Director(laptopBuilder);
+  laptopDirector.highSpecConstruct();
+  const laptopComputer = laptopBuilder.getResult();
+  console.log(laptopComputer);
+}
+
+run();
