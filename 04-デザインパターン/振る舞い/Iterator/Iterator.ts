df8513e7a88cfd9a9b8b5a487a0cb350e780645c
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-09-26 00:37:34 +0900

    Iterator途中まで

--- 04-デザインパターン/振る舞い/Iterator/Iterator.ts
+++ 04-デザインパターン/振る舞い/Iterator/Iterator.ts
@@ -5,3 +5,16 @@ export {}
 - 集約オブジェクトの要素に順番にアクセスする方法を提供する
 */
 
+
+class Patient {
+  constructor(public id: number, public name: string) {}
+}
+
+interface IIterator {
+  hasNext(): boolean
+  next(): Object
+}
+
+interface Aggregate {
+  getIterator(): IIterator
+}
