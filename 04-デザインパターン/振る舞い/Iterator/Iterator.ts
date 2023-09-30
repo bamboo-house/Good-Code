5d59b071675ffb00fea5cbda7a089e02e4824933
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-09-30 20:59:13 +0900

    Iterator実装

--- 04-デザインパターン/振る舞い/Iterator/Iterator.ts
+++ 04-デザインパターン/振る舞い/Iterator/Iterator.ts
@@ -12,9 +12,65 @@ class Patient {
 
 interface IIterator {
   hasNext(): boolean
-  next(): Object
+  next(): any
 }
 
 interface Aggregate {
   getIterator(): IIterator
 }
+
+class WaitingRoom implements Aggregate {
+  private patients: Patient[] = [];
+
+  getPatinets(): Patient[] {
+    return this.patients;
+  }
+
+  getCount(): number {
+    return this.patients.length;
+  }
+
+  checkIn(patient: Patient): void {
+    this.patients.push(patient);
+  }
+
+  getIterator(): IIterator {
+    return new WaitingRoomIterator(this);
+  }
+}
+
+class WaitingRoomIterator implements IIterator {
+  private position: number = 0;
+
+  constructor(private aggregate: WaitingRoom) {}
+
+  hasNext(): boolean {
+    return this.position < this.aggregate.getCount();
+  }
+
+  next() {
+    if (!this.hasNext()) {
+      console.log('患者がいません');
+      return;
+    }
+
+    const patient = this.aggregate.getPatinets()[this.position];
+    this.position++;
+    return patient;
+  }
+}
+
+function run() {
+  const waitingRoom = new WaitingRoom();
+  waitingRoom.checkIn(new Patient(1, 'Yamada'));
+  waitingRoom.checkIn(new Patient(2, 'Suzuki'));
+  waitingRoom.checkIn(new Patient(3, 'Tanaka'));
+
+  const iterator = waitingRoom.getIterator();
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+}
+
+run();
