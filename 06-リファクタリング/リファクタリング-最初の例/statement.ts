d9716a76358970632d1686dcf8fb8f364d1c17d0
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-10-12 16:53:16 +0900

    冠詞をつける

--- 06-リファクタリング/リファクタリング-最初の例/statement.ts
+++ 06-リファクタリング/リファクタリング-最初の例/statement.ts
@@ -31,22 +31,22 @@ export function statement(invoice: Invoices, plays: Plays) {
   return result
 
 
-  function amountFor(perf: Perf, play: Play): number {
+  function amountFor(aPerformance: Perf, play: Play): number {
     let result = 0
 
     switch (play.type) {
       case "tragedy":
         result = 40000;
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30)
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
         }
         break
       case "comedy":
         result = 30000;
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20)
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
         }
-        result += 300 * perf.audience
+        result += 300 * aPerformance.audience
         break
       default:
         throw new Error(`unknown type: ${play.type}`)
