460f39f5a37e19745fa1e7e3a9a71d946d7b8ec3
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-10-15 20:07:08 +0900

    totalVolumeCredits変数のインライン化

--- 06-リファクタリング/リファクタリング-最初の例/statement.ts
+++ 06-リファクタリング/リファクタリング-最初の例/statement.ts
@@ -13,9 +13,8 @@ export function statement(invoice: Invoices, plays: Plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
   result += `Amount owed is ${usd(totalAmount)}\n`
-  result += `You earned ${volumeCredits} credits\n`
+  result += `You earned ${totalVolumeCredits()} credits\n`
   return result
 
   function usd(aNumber: number) {
