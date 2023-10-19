409080969f81ee0baa3225a6dcb500fe5dedc554
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2023-10-19 17:54:48 +0900

    volumeCreditsをドメイン知識よりオーバーライドする設計にする

--- 06-リファクタリング/リファクタリング-最初の例/createStatementData.ts
+++ 06-リファクタリング/リファクタリング-最初の例/createStatementData.ts
@@ -8,11 +8,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.aPerformance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.aPerformance.audience / 5);
-    return result;
+    return Math.max(this.aPerformance.audience - 30, 0);
   }
 }
 
@@ -34,6 +30,10 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.aPerformance.audience;
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.aPerformance.audience / 5);
   }
 }
 
